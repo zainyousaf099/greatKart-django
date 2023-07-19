b0be9b5ddc95f2e6def3f9da7894320cf59ed158
zain <zainyousaf099@gmail.com>
2023-07-19 12:01:17 +0500

    Added try-except block to handle UserProfile.DoesNotExist

--- account/views.py
+++ account/views.py
@@ -174,14 +174,21 @@ def activate(request,uidb64,token):
 
 @login_required(login_url='login')
 def dashboard(request):
-     orders       = Order.objects.order_by('-created_at').filter(user_id=request.user.id,is_ordered=True)
-     orders_count = orders.count()
-     userprofile = UserProfile.objects.get(user_id = request.user.id)
-     context ={
-         'orders_count':orders_count,
-         'userprofile':userprofile,
-     }
-     return render(request,'accounts/dashboard.html',context)
+    orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
+    orders_count = orders.count()
+
+    try:
+        userprofile = UserProfile.objects.get(user_id=request.user.id)
+    except UserProfile.DoesNotExist:
+        # If UserProfile does not exist for the user, create a new one or handle the situation as needed.
+        userprofile = None
+
+    context = {
+        'orders_count': orders_count,
+        'userprofile': userprofile,
+    }
+    return render(request, 'accounts/dashboard.html', context)
+
 
 def forgotPassword(request):
      
