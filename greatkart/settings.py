6e0c7bf4b3d973da4eafc2b6ef5607e86b8fa325
zain <zainyousaf099@gmail.com>
2023-07-19 13:07:50 +0500

    ALLOWED HOST

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True ,cast=bool)#True
 
-ALLOWED_HOSTS = ['zain-GReatkart-env.eba-yum2pmpq.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 #  greatKart-course-env.eba-a7aervi5.us-west-2.elasticbeanstalk.com
 # Application definition
